8c7b4e699a6e27465740b796695d9b544dc5090b
SCP002 <feedusfeedme@gmail.com>
2018-01-07 14:48:28 +0300

    Added new script.
    Added new script. I use it to archivate and upload backups to the Google Drive storage.

---
+++ Create_Backup.cmd
@@ -0,0 +1,73 @@
+@Echo Off
+ChCp 65001
+SetLocal DisableDelayedExpansion EnableExtensions
+Title %~0
+
+
+Echo Before first run on the new OS, run "GDrive.exe list" to accept API key
+
+
+:: Variables
+:: ---------------------------------------------------------------------------------------------
+:SetVariables
+Set /P ArchivePwd=Archive password: 
+
+Set Utils_Path=D:\Programs
+
+Set Date=%Date:~-10%
+Set Date=%Date:/=.%
+
+
+:: Cleanup
+:: ---------------------------------------------------------------------------------------------
+:Cleanup
+Echo.
+For /D %%I In (
+    "D:\Games\Minecraft\Instances\*"
+) Do (
+    RD /S /Q "%%I\backups" "%%I\crash-reports" "%%I\logs" ^
+    & Erase /F /S /Q /A "%%I\hs_err_pid*.log" "%%I\journeymap\data\Death*.json" "%%I\saves\inventory-*-death-*.dat" "%%I\saves\inventory-*-grave-*.dat"
+)
+
+
+:: Archivate
+:: ---------------------------------------------------------------------------------------------
+:Archivate
+Echo.
+Start "7-Zip" /D "%ProgramFiles%\7-Zip" /B /Wait "%ProgramFiles%\7-Zip\7z.exe" a -p%ArchivePwd% -r -sccUTF-8 -ssw -tzip -y -- "D:\%Date%.zip" "D:\Downloads" "D:\Drivers" "D:\Games\Minecraft" "D:\Information" "D:\Installers" "D:\Programs" "D:\Projects" "D:\Scripts"
+
+
+::DeleteOld
+::Using a `Call` command with the label inside the `For ... In (...)` does not work in CMD (without big workarounds), so no something like `Call :ExecuteCommand -args` used below.
+:: ---------------------------------------------------------------------------------------------
+:DeleteOld
+Echo.
+For /F "UseBackQ Tokens=1 Delims= " %%I In (
+    `Start "GDrive" /D "%Utils_Path%" /B /Wait "%Utils_Path%\GDrive.exe" list --absolute ^
+        ^| FindStr /I "Backups.*zip"`
+) Do (
+    Start "GDrive" /D "%Utils_Path%" /B /Wait "%Utils_Path%\GDrive.exe" delete "%%I"
+)
+
+
+::UploadNew
+:: ---------------------------------------------------------------------------------------------
+:UploadNew
+Echo.
+For /F "UseBackQ Tokens=1 Delims= " %%I In (
+    `Start "GDrive" /D "%Utils_Path%" /B /Wait "%Utils_Path%\GDrive.exe" list --absolute ^
+        ^| FindStr /I "Backups"`
+) Do (
+    Start "GDrive" /D "%Utils_Path%" /B /Wait "%Utils_Path%\GDrive.exe" upload --parent "%%I" --delete "D:\%Date%.zip"
+)
+
+
+:: Exit
+:: ---------------------------------------------------------------------------------------------
+:Exit
+PowerShell -Command "& { [System.Console]::Beep(500, 1000); }"
+Color 0A
+Echo.
+Echo Done. Press any key to exit...
+Pause >Nul
+Exit /B 0
