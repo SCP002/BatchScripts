3df54f282c120179a6b141344ec654e18fdbeaa0
SCP002 <feedusfeedme@gmail.com>
2018-11-02 22:11:36 +0300

    Add extensions and split them to categories in 'Install_VSCode_Extensions.cmd'

--- Install_VSCode_Extensions.cmd
+++ Install_VSCode_Extensions.cmd
@@ -21,19 +21,33 @@ Set VSCode_Path=%LocalAppData%\Programs\Microsoft VS Code\bin
 Echo.
 Echo InstallExtensions...
 
+:: Angular
 Call "%VSCode_Path%\code.cmd" "--install-extension" "Angular.ng-template"
+
+:: General
 Call "%VSCode_Path%\code.cmd" "--install-extension" "christian-kohler.path-intellisense"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "dbaeumer.vscode-eslint"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "ecmel.vscode-html-css"
 Call "%VSCode_Path%\code.cmd" "--install-extension" "EditorConfig.EditorConfig"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "eg2.tslint"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "eg2.vscode-npm-script"
 Call "%VSCode_Path%\code.cmd" "--install-extension" "Gruntfuggly.todo-tree"
 Call "%VSCode_Path%\code.cmd" "--install-extension" "k--kato.intellij-idea-keybindings"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "michelemelluso.code-beautifier"
-Call "%VSCode_Path%\code.cmd" "--install-extension" "mike-co.import-sorter"
 Call "%VSCode_Path%\code.cmd" "--install-extension" "rokoroku.vscode-theme-darcula"
 Call "%VSCode_Path%\code.cmd" "--install-extension" "Tyriar.sort-lines"
+
+:: PHP
+Call "%VSCode_Path%\code.cmd" "--install-extension" "felixfbecker.php-intellisense"
+
+:: Python
+Call "%VSCode_Path%\code.cmd" "--install-extension" "ms-python.python"
+Call "%VSCode_Path%\code.cmd" "--install-extension" "VisualStudioExptTeam.vscodeintellicode"
+
+:: TypeScript
+Call "%VSCode_Path%\code.cmd" "--install-extension" "eg2.tslint"
+Call "%VSCode_Path%\code.cmd" "--install-extension" "mike-co.import-sorter"
+
+:: Web
+Call "%VSCode_Path%\code.cmd" "--install-extension" "dbaeumer.vscode-eslint"
+Call "%VSCode_Path%\code.cmd" "--install-extension" "ecmel.vscode-html-css"
+Call "%VSCode_Path%\code.cmd" "--install-extension" "eg2.vscode-npm-script"
+Call "%VSCode_Path%\code.cmd" "--install-extension" "michelemelluso.code-beautifier"
 
 
 
