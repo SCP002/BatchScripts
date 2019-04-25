4e5bde7fcd053e50edbcf79fe791a3d8ec22173f
SCP002 <feedusfeedme@gmail.com>
2019-04-25 08:19:20 +0300

    Return Minecraft-related scripts back

---
+++ Build_MC_Mod.cmd
@@ -0,0 +1,54 @@
+@Echo Off
+ChCp 65001
+SetLocal DisableDelayedExpansion EnableExtensions
+Title %~0
+
+
+
+:: SetVariables
+:: ---------------------------------------------------------------------------------------------
+:SetVariables
+Echo Press ^<Enter^> to set default value
+Set /P Project_Path=Project path: 
+
+If "%Project_Path%" Equ "" (
+    Set Project_Path=D:\Projects\DropOff
+)
+
+CD /D "%Project_Path%"
+
+
+Set Build_Path=%Project_Path%\build\libs
+
+
+Echo.
+Echo Project path: %Project_Path%
+Echo.
+Pause
+
+
+
+:: Build
+:: ---------------------------------------------------------------------------------------------
+:Build
+Echo.
+Erase /F /Q /A "%Build_Path%\*.jar"
+Erase /F /Q /A "%Project_Path%\*.jar"
+
+
+Call "%Project_Path%\gradlew.bat" build
+
+
+XCopy "%Build_Path%\*.jar" "%Project_Path%\" /C /I /Q /G /H /R /K /Y /Z >Nul
+
+
+
+:: Exit
+:: ---------------------------------------------------------------------------------------------
+:Exit
+PowerShell -Command "& { [System.Console]::Beep(500, 1000); }"
+Color 0A
+Echo.
+Echo Done. Press any key to exit...
+Pause >Nul
+Exit /B 0
