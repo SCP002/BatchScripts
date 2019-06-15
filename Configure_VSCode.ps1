e3b39eb79be11706ced97b63fca62ddb464b4efc
SCP002 <feedusfeedme@gmail.com>
2019-06-16 01:34:48 +0300

    Remove 'Installing extenisons...' console spam in 'Configure_VSCode.ps1'

--- Configure_VSCode.ps1
+++ Configure_VSCode.ps1
@@ -129,7 +129,8 @@ function InstallExtensions {
     )
 
     foreach ($Extension in $Extensions) {
-        StartProcess -FilePath $VSCodeExecFile -ArgumentList @('--install-extension', $Extension) -Wait -DisplayOutput
+        StartProcess -FilePath $VSCodeExecFile -ArgumentList @('--install-extension', $Extension) -Wait -DisplayOutput |
+            Select-String -Pattern 'Installing extensions...' -NotMatch  # Refine output
     }
 }
 
