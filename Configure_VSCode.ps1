fd0359ed144aa28678aa6b5fe57f7441e3ab3c0f
SCP002 <feedusfeedme@gmail.com>
2021-04-30 14:52:20 +0300

    Remove redundant preference from Configure_VSCode.ps1

--- Configure_VSCode.ps1
+++ Configure_VSCode.ps1
@@ -198,7 +198,6 @@ function WriteConfig {
             'foreground' = 'green'
             'type' = 'text'
         }
-        'todo-tree.tree.showScanModeButton' = $false
         'typescript.preferences.importModuleSpecifier' = 'non-relative'
         'typescript.preferences.quoteStyle' = 'single'
         'typescript.updateImportsOnFileMove.enabled' = 'always'
